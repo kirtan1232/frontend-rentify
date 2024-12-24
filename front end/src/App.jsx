6f42e49503e4650c73c8dcdbfc0cc5b2da4f3d21
Kirtan Shrestha <shresthakirtan4@gmail.com>
2024-12-24 17:44:04 +0545

    done

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,108 +1,31 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { lazy, Suspense } from "react";
-// import BookingIndex from "./core/private/booking";
-const BookingIndex = lazy(() => import("./core/private/booking"));
-
-// import CustomerForm from "./core/private/customer/form";
-const CustomerForm = lazy(() => import("./core/private/customer/form"));
-
-// import CustomerIndex from "./core/private/customer";
-const CustomerIndex = lazy(() => import("./core/private/customer"));
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import './App.css';
+import Navbar from './components/Navbar';
 
 
-// import Home from "./core/public/home"
-const Home = lazy(() => import("./core/public/home"));
+import Login from "./pages/account/login";
 
-// import Login from "./core/public/login"
-const Login = lazy(() => import("./core/public/login"));
-
-// import LoginCustomer from "./core/public/login-customer"
-const LoginCustomer = lazy(() => import("./core/public/login-customer"));
-
-// import Register from "./core/public/register"
-const Register = lazy(() => import("./core/public/register"));
-
-// import Layout from "./core/private/layout"
-const Layout = lazy(() => import("./core/private/layout"));
+import './App.css';
 
 function App() {
-  const publicRoutes = [
-    {
-      path: "/",
-      element: (
-        <Suspense>
-          <Home />
-        </Suspense>
-      ),
-      errorElement: <>error</>,
-    },
-    {
-      path: "/login",
-      element: (
-        <Suspense>
-          <Login />
-        </Suspense>
-      ),
-      errorElement: <>error</>,
-    },
-    {
-      path: "/login-customer",
-      element: (
-        <Suspense>
-          <LoginCustomer />
-        </Suspense>
-      ),
-      errorElement: <>error</>,
-    },
-    {
-      path: "/register",
-      element: (
-        <Suspense>
-          <Register />
-        </Suspense>
-      ),
-      errorElement: <>error</>,
-    },
-    { path: "*", element: <>unauthorized</> },
-  ];
-
-  const privateRoutes = [
-    {
-      path: "/admin",
-      element: (
-        <Suspense>
-          <Layout />
-        </Suspense>
-      ),
-      errorElement: <>error</>,
-      children:[
-        {
-         path:"/admin/customer" ,
-         element:<Suspense><CustomerIndex/></Suspense>,
-         errorElement: <>error</>
-        },
-        {
-          path:"/admin/customer/form" ,
-          element:<Suspense><CustomerForm/></Suspense>,
-          errorElement: <>error</>
-         },
-         {
-           path:"/admin/booking" ,
-           element:<Suspense><BookingIndex/></Suspense>,
-           errorElement: <>error</>
-          }
-      ]
-    },
-    { path: "*", element: <>Page not found</> },
-  ];
-
-  // LOGIN logic TODO
-  const isAdmin = true;
-  const routes = isAdmin ? privateRoutes : publicRoutes;
   return (
-    <>
-      <RouterProvider router={createBrowserRouter(routes)} />
-    </>
+
+    <Router>
+      <Navbar />
+      <ToastContainer />
+
+      {/* Main Content Wrapper with margin on the left and right */}
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </Router>
   );
 }
 
