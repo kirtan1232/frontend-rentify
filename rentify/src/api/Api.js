249c00475a85de90c5d1c4d8829ea3213fe0c66c
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-01-14 16:25:58 +0545

    1

--- rentify/src/api/Api.js
+++ rentify/src/api/Api.js
@@ -25,3 +25,27 @@ export const registerUserApi = (data) => api.post('/api/user/create', data);
 
 // Login user API
 export const loginUserApi = (data) => api.post('/api/user/login', data);
+
+export const updateUser = (id, data) => api.put(`/api/user/update/${id}`, data, {
+    headers: {
+        "Content-Type": "application/json", // Set Content-Type for JSON
+        'authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+});
+
+// create product create api
+export const createProductApi = (data) => api.post('/api/product/create', data)
+
+// fetch all products
+export const getAllProducts = () => api.get('/api/product/get_all_products', config)
+
+//fetch single product
+export const getSingleProduct = (id) => api.get(`/api/product/get_single_product/${id}`, config)
+
+// delete product (Task)
+export const deleteProduct = (id) => api.delete(`/api/product/delete_product/${id}`)
+
+// update product
+export const updateProduct = (id, data) => api.put(`/api/product/update_product/${id}`, data, config)
+
+export const searchProducts = (searchQuery) => api.get(`/api/product/search?q=${searchQuery}`, config);
