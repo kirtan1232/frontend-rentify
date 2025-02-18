d4821e2d374cfd0386e24be39bda6972dd2d8148
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-02-18 16:25:12 +0545

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
+
   <>
     <App />
     <ToastContainer />
