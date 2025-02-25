bef641a20d01fc54e27d0961fb354035b843f00a
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-02-25 09:04:02 +0545

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import App from "./App.jsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
 
   <>
-    <App />
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ToastContainer />
+    </QueryClientProvider>
   </>
 
   // </StrictMode>,
